0e299d251ec15ca8f002e526d5ae7a05085c9d91
Peter Hamberg <zazcallabah@gmail.com>
2014-09-29 12:52:30 +0200

    extract default presets into separate file

---
+++ defaults.js
@@ -0,0 +1,35 @@
+var _defaults = [
+
+{
+	permanent:true,
+	name:"Kill my upper body",
+	dice:[
+	{
+		name:"Action",
+		faces:[ "Double taps","Pushups","Climbups","Deadhang 5s","Traverse 0.5m",
+			"Deadhang traverse 0.5m","Flathand climbups","Handstand traverse 0.5m",
+			"Handstand pushups","Monkey walk 2m"]
+	},{
+		name:"Modifier",
+		faces:[ "of PAIN","of PAIN","of DOOM","of DOOM",
+			"of DISASTER","of DISASTER","of CHAOS","of CHAOS","of DEATH"]
+	},{
+		name:"Count",
+		faces:[ "x6","x6","x6","x6","x6","x6","x7","x8","x10"]
+	}]
+},
+
+{
+	permanent:true,
+	name:"Kill my lower body",
+	dice:[
+	{
+		name:"Action",
+		faces:[ "Duckwalk","Partner carry","Lunges","Plios","Crouch jumps"]
+	},{
+		name:"Count",
+		faces:[ "x2", "x2", "x3", "x4", "x5", "x6"]
+	}]
+},
+
+];
